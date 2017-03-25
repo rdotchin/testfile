4e89fef56ea75cc1178e84a70945b55c7d39d45a
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-25 01:07:57 -0400

    more bad code

--- 1.js
+++ 1.js
@@ -2,3 +2,19 @@ function test() {
 var a = "foo";
 var b = "bar";
 }
+
+var testObj = {
+one: "hello",
+                    two: "bye",
+three: 'three',
+    four:'four',
+ five:        'five'
+};
+
+                   console.log('TidyGit');
+
+var one = 1;
+     var two = 2;
+     
+          function add(a, b){return a + b
+          }
