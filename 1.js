5a0fb4fd8df56fc4e69a76e2caf7c9a149d8a37f
Richard Dotchin <richard.dotchin@gmail.com>
2017-04-05 23:56:53 +0000

    TidyGit

--- 1.js
+++ 1.js
@@ -1,20 +1,21 @@
 function test() {
-var a = "foo";
-var b = "bar";
+    var a = "foo";
+    var b = "bar";
 }
 
 var testObj = {
-one: "hello",
-                    two: "bye",
-three: 'three',
-    four:'four',
- five:        'five'
+    one: "hello",
+    two: "bye",
+    three: 'three',
+    four: 'four',
+    five: 'five'
 };
 
-                   console.log('TidyGit');
+console.log('TidyGit');
 
 var one = 1;
-     var two = 2;
+var two = 2;
 
-          function add(a, b){return a + b
-          }
+function add(a, b) {
+    return a + b
+}
